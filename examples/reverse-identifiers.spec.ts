5ce4b08bf9ce1552207fe5e388e6359b1060e3f7
Steven Chim <655241+chimurai@users.noreply.github.com>
2025-03-09 21:23:48 +0100

    test: applyTransform example (#223)

---
+++ examples/reverse-identifiers.spec.ts
@@ -0,0 +1,27 @@
+import outdent from 'outdent';
+import { createTestTransform } from './test-kit';
+import transformer from './reverse-identifiers';
+
+describe("reverse-identifiers", () => {
+  const transform = createTestTransform(transformer)
+
+  it('should reverse the names of all identifiers', () => {
+    const source = outdent`
+      const firstWord = 'Hello ';
+      const secondWord = 'world';
+      const message = firstWord + secondWord;
+
+      const getMessage = (): string => message;
+    `;
+
+    const expected = outdent`
+      const droWtsrif = 'Hello ';
+      const droWdnoces = 'world';
+      const egassem = droWtsrif + droWdnoces;
+
+      const egasseMteg = (): string => egassem;
+    `;
+
+    expect(transform({ source })).toEqual(expected);
+  })
+});
