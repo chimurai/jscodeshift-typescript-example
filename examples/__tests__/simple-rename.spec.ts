5087a888ba775f0ef11c20050b6c489e54be1207
Steven Chim <655241+chimurai@users.noreply.github.com>
2025-03-11 13:13:36 +0100

    ci: migrate jest to vitest (#224)

--- examples/__tests__/simple-rename.spec.ts
+++
@@ -1,5 +0,0 @@
-import { defineTest } from 'jscodeshift/src/testUtils';
-
-describe("renaming a variable", () => {
-  defineTest(__dirname, './simple-rename', null, 'simple-rename/basic', { parser: 'ts' });
-});
